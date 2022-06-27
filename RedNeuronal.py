7583d8c7a570f9a234a086682ded5eaacdfc9fae
ozel-byte <ozel-byte@gmail.com>
2022-06-27 18:23:01 -0500

    update

--- RedNeuronal.py
+++ RedNeuronal.py
@@ -2,32 +2,31 @@ import numpy as np
 import tensorflow as tf
 
 import matplotlib.pyplot as plt
+from leerDataSet import cargarDataSet
+
+from prueba import Prueba
+import seaborn as sns
     
-def grafica2(train):
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train[0])
-    plt.xlabel("limon")
-    plt.show()
+
             
 
-def crearModelo2(img,labels):
+def crearModelo2(img,labels,categoria):
     modelo = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32,(3,3),activation='sigmoid', input_shape=(100, 100,3)),
         tf.keras.layers.MaxPooling2D((2,2)),
         tf.keras.layers.Conv2D(64,(3,3),activation='sigmoid'),
         tf.keras.layers.MaxPooling2D((2,2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='sigmoid'),
-        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(64, activation='sigmoid'),
-        tf.keras.layers.Dense(20, activation="softmax")
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(10, activation="softmax")
 
     ])
 
     modelo.compile(
         optimizer='adam',
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=['accuracy']
     )
    
@@ -37,10 +36,39 @@ def crearModelo2(img,labels):
     history=modelo.fit(
         img,
         labels,
-        epochs=40,
+        epochs=100,
         
     )
-    plt.plot(history.history['loss'])
+    Prueba(modelo,categoria)
+    plt.plot(history.history['loss'],label="loss")
+    plt.title("loss")
     plt.show()
-    
+    graficar(modelo,history)
+
+
+    pass
+
+
+def graficar(modelo,historial):
+    names = ["chula","croton","limon","ixora","mango"]
+    imagenes,labels,categoria = cargarDataSet("test")
+    audio = np.array(imagenes)
+    labels = np.array(labels)
+    y1 = np.argmax(modelo.predict(audio), axis=1)
+    y2 = labels
+    cfmtx = tf.math.confusion_matrix(y1,labels)
+    fig = plt.figure(figsize=(10,8))
+    sns.heatmap(
+        cfmtx,
+        xticklabels= names,
+        yticklabels= names,
+        annot=True,
+        cmap='icefire',
+        fmt='g'
+    )
+    plt.xlabel('Predicción')
+    plt.ylabel('Verdadero')
+    plt.show()
+    fig = plt.figure(figsize=(12, 7))
+    print("llego aqui grafica matriz")
     pass
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 
 
-from RedNeuronal import crearModelo2, grafica2
+from RedNeuronal import crearModelo2
 from leerDataSet import cargarDataSet
 
 
 if __name__ == "__main__":
     imagenes,labels,categoria = cargarDataSet("train")
-    crearModelo2(imagenes,labels)
+    
+    crearModelo2(imagenes,labels,categoria)
+    
     pass
